aa40b71c22f76830051b034f58f63bb0c9a256e3
kadertosun <https://github.com/kadertosun>
2021-05-22 19:17:30 +0300

    Doktor bilgileri alarak kayıt oluşturma.

---
+++ KayitDoktor/KayitDoktor/Controllers/DoktorController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KayitDoktor.Models;
+
+namespace KayitDoktor.Controllers
+{
+    public class DoktorController : Controller
+    {
+        // GET: Doktor
+        FormEntities Db = new FormEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+        public ActionResult DoktorEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DoktorEkle(DoktorDb Veri)
+        {
+            Db.DoktorDb.Add(Veri);
+            Db.SaveChanges();
+            return View();
+        }
+    }
+}
